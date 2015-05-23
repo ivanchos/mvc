a1acdbdd37436a3f03b94d32ecc775ddb4a25b7e
ivanchos <ivan.milinkovic@gmail.com>
2015-05-23 13:27:46 +0200

    edit and delete user data

--- models/user_model.php
+++ models/user_model.php
@@ -7,10 +7,18 @@ class User_Model extends Model
 			}
 		public function userList()
 			{
-				$sth = $this->db->prepare('SELECT id, login, role FROM users');
+				$sth=$this->db->prepare('SELECT id, login, role FROM users');
 				$sth->execute();
 				return $sth->fetchAll();
 				
+			}
+		public function userSingleList($id)
+			{
+				$sth=$this->db->prepare('SELECT id, login, role FROM users WHERE id=:id');
+				$sth->execute(array(
+					':id'=>$id
+					));
+				return $sth->fetch();
 			}
 		public function create($data)
 			{
@@ -23,5 +31,25 @@ class User_Model extends Model
 					':password'=>$data['password'],
 					':role'=>$data['role']
 				));
+			}
+		public function editSave($data)
+			{
+				$sth=$this->db->prepare('UPDATE users
+					SET login=:login, password=:password, role=:role
+					WHERE id=:id
+					');
+				$sth->execute(array(
+					':id' => $data['id'],
+					':login' => $data['login'],
+					':password' => md5($data['password']),
+					':role' => $data['role']
+				));
+			}
+		public function delete($id)
+			{
+				$sth=$this->db->prepare('DELETE FROM users WHERE id = :id');
+				$sth->execute(array(
+					':id'=>$id
+					));
 			}
 	}
