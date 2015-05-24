a0d0d4c9488a6c3cd5ed4d4291c4e2c3ed8e4673
ivanchos <ivan.milinkovic@gmail.com>
2015-05-24 19:23:34 +0200

    change hash in user model

--- models/user_model.php
+++ models/user_model.php
@@ -24,7 +24,7 @@ class User_Model extends Model
 			{
 				$this->db->insert('users', array(
 					'login'=>$data['login'],
-					'password'=>Hash::create('md5', $data['password'], HASH_PASSWORD_KEY),
+					'password'=>Hash::create('sha256', $data['password'], HASH_PASSWORD_KEY),
 					'role'=>$data['role']
 					));
 				/*
@@ -43,7 +43,7 @@ class User_Model extends Model
 			{
 				$postData=array(
 					'login'=>$data['login'],
-					'password'=>Hash::create('md5', $data['password'], HASH_PASSWORD_KEY),
+					'password'=>Hash::create('sha256', $data['password'], HASH_PASSWORD_KEY),
 					'role'=>$data['role']
 					);
 				$this->db->update('users', $postData, "id={$data['id']}");
