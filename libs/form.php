756934d3810b2d1622d09470e61a7541405fbbe3
ivanchos <ivan.milinkovic@gmail.com>
2015-05-25 23:37:37 +0200

    make form

--- libs/form.php
+++ libs/form.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ *  - Fill out a form
+ *	- POST to PHP
+ *  - Sanitize
+ *  - Validate
+ *  - Return Data
+ *  - Write to Database
+ */
 class Form
 	{
 		//associative array
@@ -8,12 +15,57 @@ class Form
 			{
 				
 			}
+			
+		/**
+		 * post - This is to run $_POST
+		 * 
+		 * @param string $field - The HTML fieldname to post
+		 */
 		public function post($field)
 			{
+				//returns value of the array element with the key $field
 				$this->_postData[$field]=$_POST[$field];
+				// returns form object needed for chaining the methods in test/form.php
+				return $this;
 			}
+		
+		/**
+		 * fetch - Return the posted data
+		 * 
+		 * @param mixed $fieldName
+		 * 
+		 * @return mixed String or array
+		 */
+		public function fetch($fieldName=false)
+			{
+				if ($fieldName)
+					{
+						if (isset($this->_postData[$fieldName]))
+							{
+								// returns value of the array element with the key $fieldName
+								return $this->_postData[$fieldName];
+							}
+						else
+							{
+								return false;
+							}
+					}
+				else
+					{
+						// returns array, it's not a function
+						return $this->_postData;
+					}
+			}
+		
+		/**
+		 * val - This is to validate
+		 * 
+		 * @param string $typeOfValidator A method from the Form/Val class
+		 * @param string $arg A property to validate against
+		 */
 		public function val()
 			{
-				
+				// returns form object needed for chaining the methods in test/form.php
+				return $this;
 			}
 	}
---
+++ test/form.php
@@ -0,0 +1,31 @@
+<?php
+require '../libs/Form.php';
+//$_REQUEST is superglobal that contains $_GET, $_POST, $_COOKIE
+if (isset($_REQUEST['run']))
+	{
+		/*
+		returns form object made by array $_postData like:
+		Form Object ( [_postData:Form:private] => Array( ) ) 
+		it's empty array at the begining and after submit the form:
+		Form Object ( [_postData:Form:private] => Array ( [name] => John [age] => 18 [gender] => m ) )   for example
+		*/
+		$form=new Form();
+		$form	->post('name')
+				->post('age')
+				->post('gender');
+		
+		$a=$form->fetch();
+		$b=$form->fetch('age');
+		print_r($a);
+		echo $b;
+	}
+?>
+<form method="post" action="?run">
+	Name	<input type="text" name="name" />
+	Age 	<input type="text" name="age" />
+	Gender 	<select name="gender">
+				<option value="m">Male</option>
+				<option value="f">Female</option>
+			</select>
+			<input type="submit" value="Submit"/>
+</form>
